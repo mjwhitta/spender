ee032240f0eabacca630ae05e07af8749d658215
Miles Whittaker <mj@whitta.dev>
2025-11-23 19:13:17 -0500

    Updated deps

--- globals.go
+++ globals.go
@@ -5,7 +5,7 @@ import "regexp"
 type set struct{}
 
 // Version is the package version.
-const Version string = "0.2.5"
+const Version string = "0.2.6"
 
 var (
 	trimBegin *regexp.Regexp = regexp.MustCompile(
